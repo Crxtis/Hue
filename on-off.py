99b652784df92a9d5eae672f9c338c8d0c1f2b89
Crxtis <68164072+Crxtis@users.noreply.github.com>
2021-01-09 10:55:40 -0500

    Add files via upload

---
+++ on-off.py
@@ -0,0 +1,23 @@
+import requests as reqs
+import json
+ip = "192.168.0.62"
+username = "yjtesHyWJDPH6JaSE67iXDxhZNqXCM4UXNO34pbq"
+curtis = "16"
+fullurl = ("http://" + ip + "/api/" + username + "/lights/" + curtis )
+updateurl = ("http://" + ip + "/api/" + username + "/lights/" + curtis + "/state" )
+
+response = reqs.get(fullurl)
+jsonstring = response.text
+jsonObject = json.loads(jsonstring)
+state = jsonObject['state']['on']
+print(state) 
+
+
+if (state == True)
+    statebulb = '{"on":false}'
+    x = reqs.put(updateurl , data = statebulb)
+else:
+    statebulb = '{"on":true}'
+    x = reqs.put(updateurl , data = statebulb)
+
+        
